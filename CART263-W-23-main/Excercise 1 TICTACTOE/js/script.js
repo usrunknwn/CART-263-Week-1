879d4331964190602002409eab8f551094cb1f98
WDEM! <bluejays113@gmail.com>
2023-02-05 16:37:51 -0500

    ah
    thisisgood

--- CART263-W-23-main/Excercise 1 TICTACTOE/js/script.js
+++ CART263-W-23-main/Excercise 1 TICTACTOE/js/script.js
@@ -2,147 +2,281 @@
 Pong
 Damian Larice
 
-This is a tic tac toe game created on p5js for CART 263 project 1
+Week 3
 */
 
 "use strict";
+/* 
 
+FIRST PRACTICE WITH TWO BALLS MOVING 
 
-
-var clicks = 0;
-
-//variables for the buttons
-let reset;
-let p1score =0;
-let p2score =0;
-
-//players
-let p1;
-let p2;
-
-//score initial values
-let xscore;
-let oscore;
-
-
-//board variables
-let size;
-var square = "white"; 
-var state = [square, square, square, square, square, square, square, square, square];
-
-
-
-
-function preload() {
-
-
-}
-
-
-
-
+let ball = []; // Declare object
+let ball2;
 function setup() {
+ createCanvas(400, 400);
+ ball = new Particle();
+ ball2 = new Particle();
+}
+function draw() {
+ background(200, 50, 100);
+ ball.move();
+ ball2.move();
+ ball.display();
+ ball2.display();
+}
+class Particle {
+ constructor() {
+ this.x = random(height);
+ this.y = random(width);
+ this.speed = 5;
+ }
+  move() {
   
-  createCanvas(1500, 1500);
-  size = width / 3;
+ this.x += random(-this.speed, this.speed);
+ this.y += random(-this.speed, this.speed);
+ }
+ display() {
+ ellipse(this.x, this.y, 10);
+ }
 
-// tellin the program to fill with 3 white squares on a row and column
-  background(220);
-  var index = 0; 
-  for (var row = 0; row < 3; row++) {
-    for (var column = 0; column < 3; column++) {
-      // 1 big black square with many white squares
-        fill(state[index]);
-        rect(column*size, row*size, size, size);
-        index = index + 1;
-    }
-  }
-  //button for board reset
-  push();
-  reset = createButton('reset');
-  reset.size(200,100);
-  reset.style("font-family", "Bodoni");
-  reset.style("font-size", "48px");
-  reset.position(0, 0);
-  reset.mousePressed(changeBG);
-  pop();
+ }
+*/
 
+/*
 
-//button for X Score tracker
-  push();
-  xscore = createButton('X SCORE');
-  xscore.size(200,150);
-  xscore.style("font-family", "Bodoni");
-  xscore.style("font-size", "48px");
-  xscore.position(250, 0);
-  xscore.mousePressed(changeP1S);
-  pop();
+BALLS MOVING AROUND
 
-
-//button for Y Score tracker
-  push();
-  oscore = createButton('O SCORE');
-  oscore.size(200,150);
-  oscore.style("font-family", "Bodoni");
-  oscore.style("font-size", "48px");
-  oscore.position(500, 0);
-  oscore.mousePressed(changeP2S);
-  pop();
-
-
-
-  
-
-
+let ball = []; // Declare array
+function setup() {
+ createCanvas(400, 400);
+ for(let i = 0; i < 50; i++){
+ ball[i] = new Particle();
+ }
+}
+ 
+function draw() {
+ background(200,50,100);
+ for(let i = 0; i < ball.length; i++){
+ ball[i].move();
+  ball[i].display();
+  //showing the ball values
+  print(ball[i].x);
+ }
 }
 
+class Particle {
+ constructor() {
+ this.x = random(height);
+ this.y = random(width);
+ this.speed = 5;
+ //changing size of balls, diameter and speed arent definied because only for this instance (they are like let statements)
+ this.diameter = random(50);
+ }
+  move() {
+  
+ this.x += random(-this.speed, this.speed);
+ this.y += random(-this.speed, this.speed);
+ }
+ display() {
+  //changes color based on location
+  noStroke();
+  fill(this.x, this.y, this.diameter);
+ ellipse(this.x, this.y, this.diameter);
+ }
 
+ }
+
+ */
+
+/*
+
+THIS IS THE PRACTICE WE DID IN CLASS:
+ - WITH MAKING CHANGE OVER TIME IN COLOR
+ - MAKING SHAKE WITH MOUSE Y & Y
+ - CLICK DISSAPERE
+ - SPACE MAKE APPEAR
+
+
+let ball = []; // Declare array
+function setup() {
+  createCanvas(800, 800);
+  for (let i = 0; i < 50; i++) {
+    ball[i] = new Particle();
+  }
+}
 
 function draw() {
-
-}
-
-
-function mousePressed(){
-  
-//odd place O and on even clicks place X
-  clicks++;
-  if (clicks%2 ){
-    textSize(400);
-    fill(0,0,0);
-    textAlign(CENTER, CENTER);
-    text('X', mouseX,mouseY);
-  } else {
-    textSize(400);
-    fill(0,0,0);
-    textAlign(CENTER, CENTER);
-    text('O', mouseX,mouseY);
+  background(100, 50, 90);
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].move();
+    ball[i].display();
+    //showing the ball values
   }
- 
-
 }
-//clear board by replacing it (drawing over)
-function  changeBG(){
-  var index = 0; 
-  for (var row = 0; row < 3; row++) {
-    for (var column = 0; column < 3; column++) {
-      // 1 big black square with many white squares
-        fill(state[index]);
-        rect(column*size, row*size, size, size);
-        index = index + 1;
+
+class Particle {
+  constructor() {
+    this.x = random(height);
+    this.y = random(width);
+    this.speed = 5;
+    //changing size of balls, diameter and speed arent definied because only for this instance (they are like let statements)
+    this.diameter = random(50);
+    this.timeAlive = 0;
+  }
+  move() {
+    // map changes range from canvas size to 1 - 10, then with random we make the speed change based on mouse x and y
+    var x = map(mouseX, 0, width, 1, 10);
+    var y = map(mouseY, 0, height, 1, 10);
+    this.x += random(-x, x);
+    this.y += random(-y, y);
+
+    // each time move function operates time alive goes up by a value (time)
+    this.timeAlive += 0.001;
+  }
+  display() {
+    //changes color based on location
+    noStroke();
+    //* time.alive because its changing independent var by the time elapsed
+    fill(
+      this.x * this.timeAlive,
+      this.y * this.timeAlive,
+      this.diameter * this.timeAlive
+    );
+    ellipse(this.x, this.y, this.diameter);
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < ball.length; i++) {
+    var currentBall = ball[i];
+    if (
+      //if mouse potition is bigger than one of the ball right or left side for x and top and bottom for y but not past the diameter
+      mouseX >= currentBall.x &&
+      mouseX <= currentBall.x + currentBall.diameter &&
+      mouseY >= currentBall.y &&
+      mouseY <= currentBall.y + currentBall.diameter
+    ) {
+      //start at "i" and delete "1". Splice defines
+      ball.splice(i, 1);
     }
   }
 }
 
-//the "html" changes the text inside the button that is from the html file
-function changeP1S(){
-  p1score = p1score+1;
-  xscore.html(p1score);
+function keyPressed() {
+  if (keyCode == 32) {
+    //push adds 1 into an array
+    ball.push(new Particle());
+  }
+}
+// touch screen personality, touch things to make dissapere
+*/
+
+var change = 1;
+var diam = 30;
+var t;
+
+var tf = false;
+
+let ball = []; // Declare array
+function setup() {
+  createCanvas(1000, 1000);
+  for (let i = 0; i < 500; i++) {
+    ball[i] = new Particle();
+  }
+  t = 0;
+}
+
+function draw() {
+  /*
+  background(110, 112, 115);
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].move();
+    ball[i].display();
+    //showing the ball values
+  }
+  */
+  stroke(0, 18);
+  fill(18, 18, 18);
+
+  var x1 = width * noise(t + 0);
+  var x2 = width * noise(t + 25);
+  var x3 = width * noise(t + 35);
+  var x4 = width * noise(t + 45);
+  var y1 = height * noise(t + 55);
+  var y2 = height * noise(t + 65);
+  var y3 = height * noise(t + 75);
+  var y4 = height * noise(t + 85);
+
+  // diameter change
+  diam += change;
+  if (diam > 30) {
+    change = -change;
+  } else if (diam < 20) {
+    change = -change;
+  }
+
+  stroke(0, 18);
+  fill(50, 50, 50);
+  rect(x1, y1, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(70, 60, 60);
+  rect(x2, y2, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(80, 80, 80);
+  rect(x3, y3, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(70, 60, 60);
+  rect(x4, y4, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+
+  t += 0.0007;
+
+
 
 }
 
-function changeP2S(){
-  p2score = p2score+1;
-  oscore.html(p2score);
+function mousePressed() {
+  background(110, 112, 115);
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].move();
+    ball[i].display();
+    
+    
+    //showing the ball values
+  }
+}
 
+class Particle {
+  // constuctor is first instance called once (class setup)
+  constructor() {
+    this.x = random(height);
+    this.y = random(width);
+    this.speed = 5;
+    this.diam = 30;
+    //changing size of balls, diameter and speed arent definied because only for this instance (they are like let statements)
+
+    this.timeAlive = 0;
+  }
+  move() {
+    // each time move function operates time alive goes up by a value (time)
+    this.timeAlive += 0.01;
+  }
+  display() {
+    //changes color based on location
+    noStroke();
+    //* time.alive because its changing independent var by the time elapsed
+    fill(0, 30 - this.timeAlive, 0);
+    rect(this.x, this.y, this.diam, 90, 10);
+
+    // diameter change
+    this.diam += change;
+    if (this.diam > 50) {
+      change = -change;
+    } else if (this.diam < 0) {
+      change = -change;
+    }
+  }
 }
--- CART263-W-23-main/Project 1/js/script.js
+++ CART263-W-23-main/Project 1/js/script.js
@@ -4,7 +4,7 @@ Damian
 */
 
 "use strict";
-
+/*
 let p1Score = 0; 
 let p2Score = 0;
 let Paddle1Y;
@@ -106,25 +106,112 @@ function keyPressed(){
 }
 */
 
-function movePaddle(){
-    if(keyIsDown(UP_ARROW)){
-        Paddle2Y -= paddleSpeed;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        Paddle2Y += paddleSpeed;
-    }
+var change = 1;
+var diam = 30;
+var t;
 
-  if(keyIsDown(87)){
-        Paddle1Y -= paddleSpeed;
-    }
-    if(keyIsDown(83)){
-        Paddle1Y += paddleSpeed;
-    }
+var tf = false;
+
+let ball = []; // Declare array
+function setup() {
+  createCanvas(1000, 1000);
+  for (let i = 0; i < 500; i++) {
+    ball[i] = new Particle();
+  }
+  t = 0;
 }
 
-function drawScore(){
-    textSize(50);
-    textAlign(CENTER);
-    text(p1Score + "-" + p2Score, wudth/2, 100);
+function draw() {
+  beginLayer();
+  background(110, 112, 115);
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].move();
+    ball[i].display();
+    //showing the ball values
+  }
+  endLayer();
+
+  beginLayer();
+  stroke(0, 18);
+  fill(18, 18, 18);
+
+  var x1 = width * noise(t + 0);
+  var x2 = width * noise(t + 25);
+  var x3 = width * noise(t + 35);
+  var x4 = width * noise(t + 45);
+  var y1 = height * noise(t + 55);
+  var y2 = height * noise(t + 65);
+  var y3 = height * noise(t + 75);
+  var y4 = height * noise(t + 85);
+
+  // diameter change
+  /*
+  diam += change;
+  if (diam > 30) {
+    change = -change;
+  } else if (diam < 20) {
+    change = -change;
+  }
+*/
+  stroke(0, 18);
+  fill(50, 50, 50);
+  rect(x1, y1, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(70, 60, 60);
+  rect(x2, y2, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(80, 80, 80);
+  rect(x3, y3, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+  stroke(0, 18);
+  fill(70, 60, 60);
+  rect(x4, y4, diam, diam, 5);
+  rect(x1, y1, diam, diam, 5);
+
+  t += 0.0007;
+  endLayer();
 }
 
+function mousePressed() {
+  background(110, 112, 115);
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].move();
+    ball[i].display();
+
+    //showing the ball values
+  }
+}
+
+class Particle {
+  // constuctor is first instance called once (class setup)
+  constructor() {
+    this.x = random(height);
+    this.y = random(width);
+    this.speed = 5;
+    this.diam = 30;
+    //changing size of balls, diameter and speed arent definied because only for this instance (they are like let statements)
+
+    this.timeAlive = 0;
+  }
+  move() {
+    // each time move function operates time alive goes up by a value (time)
+    this.timeAlive += 0.01;
+  }
+  display() {
+    //changes color based on location
+    noStroke();
+    //* time.alive because its changing independent var by the time elapsed
+    fill(0, 30 - this.timeAlive, 0);
+    rect(this.x, this.y, this.diam, 90, 10);
+
+    // diameter change
+    this.diam += change;
+    if (this.diam > 50) {
+      change = -change;
+    } else if (this.diam < 0) {
+      change = -change;
+    }
+  }
+}
